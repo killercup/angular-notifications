2019046e19ed7c37bdee9d0cf741d1d482bebd3e
Pascal Hertleif <killercup@gmail.com>
2014-02-07 19:46:20 +0100

    Generated documentation for c91d38a36285ab2328eb4498a8be94789a2607cc

---
+++ toc.js
@@ -0,0 +1,51 @@
+window.files = [
+{
+  "path": "index.html",
+  "originalName": "README.md",
+  "originalPath": "README.md",
+  "name": "index.html",
+  "title": "Angular Notifications Service",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "angular-notifications-service",
+      "title": "Angular Notifications Service"
+    },
+    {
+      "level": 2,
+      "slug": "install",
+      "title": "Install"
+    },
+    {
+      "level": 2,
+      "slug": "usage",
+      "title": "Usage"
+    }
+  ]
+},
+{
+  "path": "src/notifications.js.html",
+  "originalName": "notifications.js",
+  "originalPath": "src/notifications.js",
+  "name": "notifications.js.html",
+  "lang": "javascript",
+  "title": "Notification Display Service",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "notification-display-service",
+      "title": "Notification Display Service"
+    },
+    {
+      "level": 2,
+      "slug": "show-alert-message",
+      "title": "Show Alert Message"
+    },
+    {
+      "level": 2,
+      "slug": "ask-for-confirmation",
+      "title": "Ask for Confirmation"
+    }
+  ]
+}
+];
